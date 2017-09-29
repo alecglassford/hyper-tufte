9def89fff42e7c2b326e2849c0d11550d73ae3db
Alec Glassford <glassford@stanford.edu>
2017-09-28 23:23:03 -0700

    🔄 Swap yellow and white for better ansi yellow highlights

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const colors = {};
 colors.black = colors.lightBlack = '#594a7d';
 colors.red = colors.lightRed = '#f35970';
 colors.green = colors.lightGreen = '#83d996';
-colors.yellow = colors.lightYellow = '#f5e566';
+colors.yellow = colors.lightYellow = '#ffffff';
 colors.blue = colors.lightBlue = '#62bff7';
 colors.magenta = colors.lightMagenta = '#f15195';
 colors.cyan = colors.lightCyan = '#48ecd3';
-colors.white = colors.lightWhite = '#ffffff';
+colors.white = colors.lightWhite = '#f5e566';
 
 exports.decorateConfig = function(config) {
   return Object.assign({}, config, {
     cursorColor: colors.blue + 'aa', // opacity of 2/3
-    foregroundColor: colors.yellow,
+    foregroundColor: colors.white,
     backgroundColor: colors.black,
     borderColor: colors.magenta,
     colors
